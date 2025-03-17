e8115096b6cd15023570fcb9494e11efd2dcbf73
Deividas <deividuks2007@gmail.com>
2025-03-17 21:06:49 +0200

    Added backend monitoring for health checking

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,28 @@
 #include <unistd.h>
 #include "../include/tcp_proxy.h" 
 #include <yaml-cpp/yaml.h>
+#include "../include/health_check.h"
+#include <thread>
 
 
 
-int main(){
+int main() {
 
+   
     load_backends("../config.yaml");
 
+    
+    std::thread health_check_thread(monitor_backends);
 
-    int server_sock = socket(AF_INET,SOCK_STREAM,0);
-    if(server_sock== -1){
+    
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock == -1) {
         std::cerr << "Failed to create server socket\n";
         return 1;
     }
-    
+
     sockaddr_in server_addr{};
-    server_addr.sin_family= AF_INET;
+    server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(LISTEN_PORT);
 
@@ -29,28 +35,36 @@ int main(){
         return 1;
     }
 
-    if(listen(server_sock, 10 )== -1){
+    if (listen(server_sock, 10) == -1) {
         std::cerr << "Listen failed\n";
         close(server_sock);
         return 1;
     }
-    
+
     std::cout << "Listening on port " << LISTEN_PORT << "...\n";
 
-    while (true){
+    
+    while (true) {
         sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
-        int client_sock = accept(server_sock,(struct sockaddr*)&client_addr,&client_len);
+        int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_len);
 
-        if (client_sock == -1){
-            std::cerr << "Accept failed \n";
+        if (client_sock == -1) {
+            std::cerr << "Accept failed\n";
             continue;
         }
 
-        std::cout << "New client connected!'\n";
+        std::cout << "New client connected!\n";
         client_handling(client_sock);
-
     }
 
+   
     close(server_sock);
+
+    
+    if (health_check_thread.joinable()) {
+        health_check_thread.join();
+    }
+
+    return 0;
 }
